6d5aa0859ce69f16397c11033594154a5fc0d67c
Dexfans <dexfansx@hotmail.com>
2026-09-02 01:23:54 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "DexFans",
-  description: "Connect with creators.",
+  title: "DexFans.world",
+  description: "The creator social platform built for Web3."
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
       <body>{children}</body>
